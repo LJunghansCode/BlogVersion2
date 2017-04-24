435f80c0a4e19e9cad15b2d5668a6fc5a0969b45
Shimmyshanga <luca.junghans@gmail.com>
2017-04-24 09:24:50 -0700

    dbchanges

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,11 +1,11 @@
 
 var mongoose = require('mongoose');
 			fs = require('fs'),
-            imgPath = './../../client/static/img'
+      imgPath = './../../client/static/img';
 			path = require('path'),
 			models_path = path.join(__dirname,"../models"),
 			reg = new RegExp( ".js$", "i" ),
-			dbURI = 'mongodb://localhost/MYBLOG';
+			dbURI = 'mongodb://lucaAdmin:BogasBlog1234@localhost/MYBLOG';
 
 
 mongoose.connect(dbURI);
