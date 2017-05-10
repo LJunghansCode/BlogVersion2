9ce8f7953303eb2a54614a7389ae3a81da40e0cc
Shimmyshanga <luca.junghans@gmail.com>
2017-05-10 10:36:02 -0700

    da

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -32,14 +32,6 @@ module.exports = (function(){
                 }
             });
         }
-
-
-
-
-
-
-
-
     };
 
 })();
@@ -48,7 +40,7 @@ module.exports = (function(){
     //         var form = new multiparty.Form();
     //             form.parse(req, function(err, fields, files) {
     //                 var file = files.file[0];
-    //                 var contentType = file.headers['content-type'];
+    //                 var contentType = fsile.headers['content-type'];
     //                 var tmpPath = file.path;
     //                 var extIndex = tmpPath.lastIndexOf('.');
     //                 var extension = (extIndex < 0) ? '' : tmpPath.substr(extIndex)
